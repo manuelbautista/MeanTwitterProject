3c87a091600d2efb212a9dc3b833deee8b43e29c
Manuel Bautista <b0hcoder@gmail.com>
2015-11-16 13:54:09 -0600

    Proyecto Twitter Mean

---
+++ package/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
+app.get('/api/posts', function (req, res) {
+
+res.json([
+		{
+			username: 'dickeyxxx',
+			body: 'node rocks!'
+		}
+	])
+})
+
+app.listen(3000, function () {
+	console.log('Server listening on', 3000)
+})
